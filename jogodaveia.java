525c2ac605f7c350839eab7657ab555f55f7e3a7
Felipe Tomio Maciel <116910843+Felipe-T-Maciel@users.noreply.github.com>
2022-12-13 00:56:52 -0300

    Update jogodaveia.java

--- jogodaveia.java
+++ jogodaveia.java
@@ -6,8 +6,6 @@ public class jogodaveia {
         int vezJogador=0,i=0,j=0;
         boolean[][] X = new boolean[3][3];
         boolean[][] O = new boolean[3][3];
-        boolean vencedorX = false;
-        boolean vencedorO = false;
         int contadorJogadas=0;
 
 
@@ -17,11 +15,9 @@ public class jogodaveia {
             }
         }
 
-        jogoDaVelha[1][1] = " x ";
-        System.out.println("\nLembre-se as coordenadas são dadas apartir do 0,\nExemplo Coluna: 1 | Linha: 1\n Como sera imprimido:");
-        System.out.println(jogoDaVelha[0][0]+"|"+jogoDaVelha[1][0]+"|"+jogoDaVelha[2][0]);
-        System.out.println(jogoDaVelha[0][1]+"|"+jogoDaVelha[1][1]+"|"+jogoDaVelha[2][1]);
-        System.out.println(jogoDaVelha[0][2]+"|"+jogoDaVelha[1][2]+"|"+jogoDaVelha[2][2]);
+        jogoDaVelha[0][1] = " x ";
+        System.out.println("\nLembre-se as coordenadas são dadas apartir do 0,\nExemplo Coluna: 0 | Linha: 1\n Como sera imprimido:");
+        imprimi(jogoDaVelha);
 
         for(i=0;i<3;i++){
             for(j=0;j<3;j++){
@@ -31,15 +27,15 @@ public class jogodaveia {
             }
         }
 
-        System.out.println("Agora é sua vez\n====== Jogo Da Velha ======");
+        System.out.println("Agora é sua vez\n\n====== Jogo Da Velha ======");
 
-        while(vencedorX==false & vencedorO==false & contadorJogadas<9){
+        while(verificaX(X)==false & verificaX(O)==false & contadorJogadas<9){
+            System.out.println("\nColuna: ");
+            i=scan.nextInt();
+            System.out.println("Linha: ");
+            j=scan.nextInt();
+            if(i>=0 && i<=2 & j>=0 && j<=2){
                 contadorJogadas++;
-                System.out.println("\nColuna: ");
-                i=scan.nextInt();
-                System.out.println("Linha: ");
-                j=scan.nextInt();
-
                 if(vezJogador%2==0){
                     if(X[i][j] == false || O[i][j] == false){
                         System.out.println("Jogada invalida");
@@ -48,9 +44,7 @@ public class jogodaveia {
                         vezJogador++;
                         jogoDaVelha[i][j] = " x ";
                         X[i][j] = false;
-                        System.out.println(jogoDaVelha[0][0]+"|"+jogoDaVelha[1][0]+"|"+jogoDaVelha[2][0]);
-                        System.out.println(jogoDaVelha[0][1]+"|"+jogoDaVelha[1][1]+"|"+jogoDaVelha[2][1]);
-                        System.out.println(jogoDaVelha[0][2]+"|"+jogoDaVelha[1][2]+"|"+jogoDaVelha[2][2]);
+                        imprimi(jogoDaVelha);
                     }
                 }
                 else{
@@ -61,81 +55,96 @@ public class jogodaveia {
                         vezJogador++;
                         jogoDaVelha[i][j] = " O ";
                         O[i][j] = false;
-                        System.out.println(jogoDaVelha[0][0]+"|"+jogoDaVelha[1][0]+"|"+jogoDaVelha[2][0]);
-                        System.out.println(jogoDaVelha[0][1]+"|"+jogoDaVelha[1][1]+"|"+jogoDaVelha[2][1]);
-                        System.out.println(jogoDaVelha[0][2]+"|"+jogoDaVelha[1][2]+"|"+jogoDaVelha[2][2]);
+                        imprimi(jogoDaVelha);
                     }
                         
                 }
-
-                if(X[0][0] == false & X[1][1] == false & X[2][2] == false){
-                    vencedorX = true;
-                }
-                if(X[2][0] == false & X[1][1] == false & X[0][2] == false){
-                    vencedorX = true;
-                }
-                if(X[0][0] == false & X[0][1] == false & X[0][2] == false){
-                    vencedorX = true;
-                }
-                if(X[1][0] == false & X[1][1] == false & X[1][2] == false){
-                    vencedorX = true;
-                }
-                if(X[2][0] == false & X[2][1] == false & X[2][2] == false){
-                    vencedorX = true;
-                }
-                if(X[0][0] == false & X[1][1] == false & X[2][2] == false){
-                    vencedorX = true;
-                }
-                if(X[0][0] == false & X[1][0] == false & X[2][0] == false){
-                    vencedorX = true;
-                }
-                if(X[0][1] == false & X[1][1] == false & X[2][1] == false){
-                    vencedorX = true;
-                }
-                if(X[0][2] == false & X[1][2] == false & X[2][2] == false){
-                    vencedorX = true;
-                }
-
-                //separa
-
-                if(O[0][0] == false & O[1][1] == false & O[2][2] == false){
-                    vencedorO = true;
-                }
-                if(O[2][0] == false & O[1][1] == false & O[0][2] == false){
-                    vencedorO = true;
-                }
-                if(O[0][0] == false & O[0][1] == false & O[0][2] == false){
-                    vencedorO  = true;
-                }
-                if(O[1][0] == false & O[1][1] == false & O[1][2] == false){
-                    vencedorO = true;
-                }
-                if(O[2][0] == false & O[2][1] == false & O[2][2] == false){
-                    vencedorO = true;
-                }
-                if(O[0][0] == false & O[1][1] == false & O[2][2] == false){
-                    vencedorO = true;
-                }
-                if(O[0][0] == false & O[1][0] == false & O[2][0] == false){
-                    vencedorO = true;
-                }
-                if(O[0][1] == false & O[1][1] == false & O[2][1] == false){
-                    vencedorO = true;
-                }
-                if(O[0][2] == false & O[1][2] == false & O[2][2] == false){
-                    vencedorO = true;
-                }
+            }
+            else{
+                System.out.println("Jogada Invalida");
+            }
         }
-        if(vencedorO == true){
-            System.out.println("Jogador 'O' venceu");
-        }
-        else if(vencedorX == true){
+
+        if(verificaX(X) == true){
             System.out.println("Jogador 'X' venceu");
+        }
+        else if(verificaX(O) == true){
+            System.out.println("Jogador 'O' venceu");
         }
         else{
             System.out.println("Velha");
         }
     }
+
+    public static void imprimi(String[][] jogoDaVelha){
+        System.out.println(jogoDaVelha[0][0]+"|"+jogoDaVelha[1][0]+"|"+jogoDaVelha[2][0]);
+        System.out.println(jogoDaVelha[0][1]+"|"+jogoDaVelha[1][1]+"|"+jogoDaVelha[2][1]);
+        System.out.println(jogoDaVelha[0][2]+"|"+jogoDaVelha[1][2]+"|"+jogoDaVelha[2][2]);
+    }
+
+    public static boolean verificaX(boolean[][] X){
+
+        if(X[0][0] == false & X[1][1] == false & X[2][2] == false){
+            return  true;
+        }
+        if(X[2][0] == false & X[1][1] == false & X[0][2] == false){
+            return  true;
+        }
+        if(X[0][0] == false & X[0][1] == false & X[0][2] == false){
+            return  true;
+        }
+        if(X[1][0] == false & X[1][1] == false & X[1][2] == false){
+            return  true;
+        }
+        if(X[2][0] == false & X[2][1] == false & X[2][2] == false){
+            return  true;
+        }
+        if(X[0][0] == false & X[1][1] == false & X[2][2] == false){
+            return  true;
+        }
+        if(X[0][0] == false & X[1][0] == false & X[2][0] == false){
+            return  true;
+        }
+        if(X[0][1] == false & X[1][1] == false & X[2][1] == false){
+            return  true;
+        }
+        if(X[0][2] == false & X[1][2] == false & X[2][2] == false){
+            return  true;
+        }
+        return false;
+    }
+
+    public static boolean verificaO(boolean[][] O){
+
+        if(O[0][0] == false & O[1][1] == false & O[2][2] == false){
+            return  true;
+        }
+        if(O[2][0] == false & O[1][1] == false & O[0][2] == false){
+            return  true;
+        }
+        if(O[0][0] == false & O[0][1] == false & O[0][2] == false){
+            return  true;
+        }
+        if(O[1][0] == false & O[1][1] == false & O[1][2] == false){
+            return  true;
+        }
+        if(O[2][0] == false & O[2][1] == false & O[2][2] == false){
+            return  true;
+        }
+        if(O[0][0] == false & O[1][1] == false & O[2][2] == false){
+            return  true;
+        }
+        if(O[0][0] == false & O[1][0] == false & O[2][0] == false){
+            return  true;
+        }
+        if(O[0][1] == false & O[1][1] == false & O[2][1] == false){
+            return  true;
+        }
+        if(O[0][2] == false & O[1][2] == false & O[2][2] == false){
+            return  true;
+        }
+        return false;
+    }
         
 }
-"# jogo-da-velha" 
+//"# jogo-da-velha"
